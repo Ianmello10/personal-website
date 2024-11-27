687cfe97364e527c5fc7c6bfce44cac80184c56e
Ianmello10 <lucasian.azul@gmail.com>
2024-11-27 11:37:12 -0300

    fix:revalidate-number

--- app/[locale]/blog/[slug]/page.tsx
+++ app/[locale]/blog/[slug]/page.tsx
@@ -7,7 +7,7 @@ import Link from "next/link";
 import { ArrowLeft } from "lucide-react";
 import PageTransition from "@/components/animate";
 
-export const revalidate = 60 * 60;
+export const revalidate = 3600;
 export const dynamicParams = true;
 
 export async function generateStaticParams() {
